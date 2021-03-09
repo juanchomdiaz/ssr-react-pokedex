ecaa59b0c6ee40db2d948152465983d688e3eca2
Juan Manuel <juanmanueldiaz85@gmail.com>
2021-03-09 19:37:15 -0300

    Replaced favicon and created new pokedex page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,33 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+
+import PokedexState from '@contexts/pokedex/PokedexState';
+import AppLayout from '@components/layout/AppLayout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="favicon.ico" />
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
+          rel="stylesheet"
+        />
+
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+          crossorigin="anonymous"
+        />
+      </Head>
+      <PokedexState>
+        <AppLayout>
+          <Component {...pageProps} />
+        </AppLayout>
+      </PokedexState>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
